5b137450ae800c43fda27127fd44139321d2ca99
Benjafo <benjafo@gmail.com>
2024-08-21 11:23:47 -0400

    Changed about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,9 +11,10 @@ const About = () => {
         <img src={profile} alt='profile picture' />
         <h1>About <menu type="context"></menu></h1>
         <p>&nbsp;&#8226;&nbsp;Graduate of Westford Academy Class of 2021</p>
-        <p>&nbsp;&#8226;&nbsp;Web developer and fourth year student at Rochester Institute of Technology Studying web development</p>
-        <p>&nbsp;&#8226;&nbsp;Trader at wholesale bullion and numismatics shop Numismatic Investments LLC in Stoneham for two years</p>
-        <p>&nbsp;&#8226;&nbsp;Solo numismatics and bullion trader for six years</p>
+        <p>&nbsp;&#8226;&nbsp;Fourth year student at Rochester Institute of Technology Studying web development</p>
+        <p>&nbsp;&#8226;&nbsp;Software engineer at the <a href="https://www.whoi.edu" target="_blank">Woods Hole Oceanographic Institution</a></p>
+        <p>&nbsp;&#8226;&nbsp;Head of Online Sales & Trader at Numismatic Investments LLC</p>
+        <p>&nbsp;&#8226;&nbsp;Software/web developer for six years & coin and bullion trader for eight years</p>
     </div>
     return (
         <Page primaryContent={services} secondaryContent={about} />
